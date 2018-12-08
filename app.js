89c72c3ade6f216bfef29fe9139a4571da404e4b
Ken Truong <kenvantruong@gmail.com>
2018-12-07 22:53:09 -0800

    Update app.js

--- app.js
+++ app.js
@@ -27,38 +27,7 @@ var rollDice = function rollDice (e) {
                
                
      
-               
-         
-               Part-time, Contract
-Miller Media LLC is a software development company located in the San Francisco Bay Area. We provide full web and mobile solutions to corporate clients to save them time and money in order to allow them to focus on their core business.
-We are looking for a talented Front-End Web Developer to join our team. This position is onsite at our office and you will work closely with the designer, other developers and project manger to create new and update existing webpage and web applications. This is a great opportunity for the front-end developer to roll up their sleeves and apply their experience in a fast-paced, dynamic work environment. The candidate will be responsible for the technical design, development, unit testing, and support of the company's web application projects. He/she should require excellent communications skills in order to explain technical concepts to non-technical associates, to be well organized, and to be capable of working both in independent and team environment.
-This is a contract to hire position based in Walnut Creek, CA. Candidate must be eligible to work in the US without sponsorship.
-Key responsibilities:
-* Work closely with project management and business development team to understand requirements and technical specifications
-* Must have extensive experience in HTML/CSS, JavaScript and PHP
-* Design and implement web UI components to support the development of multiple applications
-* Experience and understanding of responsive web content development
-* Write optimized code for maximum speed and scalability.
-* Build reusable templates, widgets and code for team.
-* Troubleshoot coding issues and bugs.
-* Collaborate with designer to incorporate visual design into functional web pages.
-* Act as subject matter expert on Web UI design and implementation
-Skills and Experience:
-* 2- 5 years minimum experience with HTML/CSS JavaScript,PHP, AJAX, JSON, CMS, Apache, Wordpress and Bootstrap.
-* Bachelor Degree in Computer Science or other relevant fields
-* 2 years minimum web site production and programming experience.
-* Experience solving cross browser compatibility, optimization, and layout issues
-* Collaborate with the backend engineering team to execute solutions within our production environment
-* Excellent written and verbal communication skills
-* Strong organizational and time management skills
-* Demonstrated ability to communicate effectively and build trusted relationships with various functions across engineers, project managers, design, customer success, etc.
-* Familiarity with distributed version control systems such as GitHub
-Bonus Points:
-* Amazon Web Services (AWS), Cloud based infrastructure systems is a plus.
-* Experience in eCommerce and retail space
-* App development experience is plus
-Job Type: Part-time
-30+ days ago - report job
+      
                
                
                
